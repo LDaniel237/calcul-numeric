cfab154c84a83601704c02afd89750274a5e05d0
LDaniel237 <lkingdaniel90@gmail.com>
2024-03-20 14:52:49 +0200

    tema 2 final

--- Tema2/Exercises.py
+++ Tema2/Exercises.py
@@ -1,16 +1,8 @@
 import numpy as np
 from numpy.linalg import det
 
-try:
-    n = int(input("Introduceti n: "))
-    t = int(input("Introduceti t: "))
-    if t < 5:
-        raise ValueError("t trebuie sa fie mai mare decat 4")
-    if n < 1:
-        raise ValueError("n trebuie sa fie mai mare decat 0")
-except ValueError as exception:
-    print(exception)
-    exit()
+n = int(input("Introduceti n: "))
+t = int(input("Introduceti t: "))
 
 e = 10 ** (-t)
 
@@ -18,44 +10,32 @@ def crout(A):
     n = len(A)
     L = np.zeros((n, n)) # Matrice de n*n cu elementele egale cu 0
     U = np.eye(n) # Matrice de n*n cu 1 pe diagonala principala
-    try:
-        for p1 in range(n):
-            for i in range(p1, n):
-                L[i, p1] = A[i, p1] - np.sum(L[i, :p1] * U[:p1, p1])
-            for i in range(p1 + 1, n):
-                if np.abs(L[p1, p1]) > e:
-                    U[p1, i] = (A[p1, i] - np.sum(L[p1, :i] * U[:i, i])) / L[p1, p1]
-                else:
-                    raise Exception(f"LU nu se poate descompune, det(A_{p1 + 1}) = 0")
-    except Exception as exception:
-        print(exception)
-        exit()
+    for p1 in range(n):
+        for i in range(p1, n):
+            L[i, p1] = A[i, p1] - np.sum(L[i, :p1] * U[:p1, p1])
+        for i in range(p1 + 1, n):
+            if np.abs(L[p1, p1]) > e:
+                U[p1, i] = (A[p1, i] - np.sum(L[p1, :i] * U[:i, i])) / L[p1, p1]
     return L, U
 
 
 def crout_restricted(A):
     n = len(A)
-    try:
-        for p in range(n):
-            for i in range(p, n):
-                A[i, p] = A_init[i, p] - np.sum(A[i, :p] * A[:p, p])
-            for i in range(p + 1, n):
-                if np.abs(A[p, p]) > e:
-                    A[p, i] = (A_init[p, i] - np.sum(A[p, :p] * A[:p, i])) / A[p, p]
-                else:
-                    raise Exception(f"LU nu se poate descompune, det(A_{p + 1}) = 0")
-    except Exception as exception:
-        print(exception)
-        exit()
+    for p in range(n):
+        for i in range(p, n):
+            A[i, p] = A_init[i, p] - np.sum(A[i, :p] * A[:p, p])
+        for i in range(p + 1, n):
+            if np.abs(A[p, p]) > e:
+                A[p, i] = (A_init[p, i] - np.sum(A[p, :p] * A[:p, i])) / A[p, p]
     return A
 
 
-print(f"Parametrii programului sunt: n = {n}, t = {t}, precession e={e}\n")
+print(f"Parametrii programului sunt: n = {n}, t = {t}, precizie e={e}\n")
 
-#A = np.random.rand(n, n)
-#b = np.random.rand(n)
-A = np.array([[2, 0, 2], [1, 2, 5], [1, 1, 7]])
-b = np.array([4, 10, 10])
+A = np.random.rand(n, n)
+b = np.random.rand(n)
+# A = np.array([[2, 0, 2], [1, 2, 5], [1, 1, 7]])
+# b = np.array([4, 10, 10])
 
 A_init = A.copy()
 b_init = b.copy()
@@ -126,26 +106,19 @@ def LU_optimized(A):
     n = len(A)
     L = np.ones(n * (n + 1) // 2)
     U = np.ones(n * (n + 1) // 2)
-    try:
-        for j in range(n):
-            for i in range(j, n):
-                L[indexare(i, j)] = A[i, j] - sum(L[indexare(i, k)] * U[indexare(k, j)] for k in range(j))
-            for i in range(j + 1, n):
-                if np.abs(L[indexare(j, j)]) > e:
-                    U[indexare(j, i)] = (A[j, i] - sum((L[indexare(j, k)] * U[indexare(k, i)]) for k in range(j))) / L[
-                        indexare(j, j)]
-                else:
-                    raise Exception(f"LU nu se poate descompune, det(A_{j + 1}) = 0")
-    except Exception as exception:
-        print(exception)
-        exit()
+    for j in range(n):
+        for i in range(j, n):
+            L[indexare(i, j)] = A[i, j] - sum(L[indexare(i, k)] * U[indexare(k, j)] for k in range(j))
+        for i in range(j + 1, n):
+            if np.abs(L[indexare(j, j)]) > e:
+                U[indexare(j, i)] = (A[j, i] - sum((L[indexare(j, k)] * U[indexare(k, i)]) for k in range(j))) / L[indexare(j, j)]
     return L, U
 
 
 L, U = LU_optimized(A_init)
 
-#print("\nVectorul L:", L)
-#print("Vectorul U:", U)
+print("\nVectorul L (optimized):", L)
+print("Vectorul U (optimized):", U)
 
 #Metoda substitutiei directe, Ly = b
 y = np.zeros_like(b, dtype=float)
